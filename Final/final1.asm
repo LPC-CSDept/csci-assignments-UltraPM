9eb4e67cf4f775446821134d16f1b87eae3e515e
UltraPM <94938486+UltraPM@users.noreply.github.com>
2021-12-09 00:05:45 -0800

    Final submit (not ready yet)

---
+++ Final/final1.asm
@@ -0,0 +1,18 @@
+# Final Exam: Question 1
+# Name: Phillip Mai
+#
+#
+
+    .data
+
+    .text
+    .globl  main
+main:
+
+
+loop:
+
+
+done:
+    li  $v0, 10     # Code 10 for exit
+    syscall         # End program
---
+++ Final/final2.asm
@@ -0,0 +1,18 @@
+# Final Exam: Question 2
+# Name: Phillip Mai
+#
+#
+
+    .data
+
+    .text
+    .globl  main
+main:
+
+
+loop:
+
+
+done:
+    li  $v0, 10     # Code 10 for exit
+    syscall         # End program
