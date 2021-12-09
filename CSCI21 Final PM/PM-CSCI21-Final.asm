d29968386b8d2f1a6e727cc13849a1e829a69257
UltraPM <94938486+UltraPM@users.noreply.github.com>
2021-12-08 19:15:33 -0800

    Update PM-CSCI21-Final.asm

--- CSCI21 Final PM/PM-CSCI21-Final.asm
+++ CSCI21 Final PM/PM-CSCI21-Final.asm
@@ -1,1 +1,19 @@
-# Final change overtime later
+# Final Exam : Question No1.
+# Name : Phillip Mai
+#
+#
+
+  .data
+
+  .text
+  .globl  main
+main:
+
+
+
+loop:
+
+
+done:
+  li  $v0, 10   # Code 10 for exit
+  syscall    # End program
