3d94edf1b0592a837943dffe2ab27e78dbeea5c8
UltraPM <94938486+UltraPM@users.noreply.github.com>
2021-12-10 10:17:13 -0800

    forgot some details

--- Final/final1.asm
+++ Final/final1.asm
@@ -3,7 +3,7 @@
 # Dec. 10, 2021
 # Make MIPS assembly program with Memory Mapped I/O that print real decimal value for the 3 digit user input through the MM I/O by using the syscall. 
 # OFF Bare Machine, OFF Enable Delayed Branches, ON Enable Mapped IO, OFF Enable Delayed Loads, OFF Load Exception Handler, ON Accept Pseudo Instruction
-# I copy from Lab-MMIO.asm
+# I copy from Lab-MMIO.asm with some changes that make it enter 3 number digits
 # 
 
     .data
