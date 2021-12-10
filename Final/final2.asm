a28a614e9ac0f2fafe17bc5ea7a9eb98726a93c7
UltraPM <94938486+UltraPM@users.noreply.github.com>
2021-12-10 11:05:35 -0800

    changes intro

--- Final/final2.asm
+++ Final/final2.asm
@@ -1,8 +1,8 @@
-
+# final2.asm
 # Final Exam: Question 2
 # Name: Phillip Mai
 # Dec. 10, 2021
-# Make MIPS assembly program with Memory Mapped I/O that print real decimal value for the 3 digit user input through the MM I/O by using the syscall. 
+# Make MIPS assembly program with Memory Mapped I/O that take the user character and print it in a console until 'q' is typed. Also, make the interrupt handler(Kernel text program)
 # OFF Bare Machine, OFF Enable Delayed Branches, ON Enable Mapped IO, OFF Enable Delayed Loads, OFF Load Exception Handler, ON Accept Pseudo Instruction
 # I copy from Lab-MMIO.asm with some changes that make it enter 3 number digits
 #
