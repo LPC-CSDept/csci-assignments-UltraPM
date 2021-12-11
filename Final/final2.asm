b6d9f008c795f99776a1f17b041271b47a07ff77
UltraPM <94938486+UltraPM@users.noreply.github.com>
2021-12-10 20:29:10 -0800

    removing code that are unnecessary to the program

--- Final/final2.asm
+++ Final/final2.asm
@@ -7,18 +7,16 @@
 # I copy from Lab-MMIO2.asm & IntIO-Kernel-Lab2.asm with some changes that make it tpyed characters in a console list until close by q
 #
 
-    .kdata                  # kernel data     
-s1: .word   10
-s2: .word   11
-new_line:   .asciiz "\n"    # newline
-		
+		.kdata                  # kernel data     
+s1:		.word   10
+s2:		.word   11
+new_line:	.asciiz "\n"    # newline
+
     .text     
     .globl  main
 main:
 
 	li	$s0, 100		# ASCII code for 'q' (Meaning if you press 'q' the program end)
-	lui	$t0, 0xFFFF
-
 
 # Example code to Enable all interrupts
     mfc0    $a0, $12      	  # read from the status register     
@@ -46,20 +44,13 @@ here:
 
 	lui		$v0, 0xFFFF			# $v0 = 0xFFFF0000 and it memory addresss to start on MMIO
 	lw		$a0, 4($v0)			# get the input key     
-
 	bne		$a0, $s0, print		# branch to print if there more than ASCII code 'q'
-
-	lw		$t1, 8($t0)			# load the output control register 
-	andi	$t1, $t1, 0x0001	# reset all bits except (least significant bit)
-	beq		$t1, $zero, print	# if not ready, then loop back
-	sw		$v0, 12($t0)		# output device is ready, so write
-	bne		$v0, $s0, print		# branch to print if there more than ASCII code 'q'
 
 	li	$v0, 10			#   print it here.      
 	syscall 			#   Note: interrupt routine should return very fast,     
 
 print:
-	li		$v0, 1				# Code to print it here.      
+	li		$v0, 11				# Code to print it here.      
 	syscall						    
 
 	li		$v0, 4
